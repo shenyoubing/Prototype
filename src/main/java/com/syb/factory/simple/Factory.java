cdd220d3799027032c1032fa8ce7bb0312b8af7d
shenyoubing <shenyoubing621@163.com>
2019-04-06 01:41:40 +0800

    factory

--- src/main/java/com/syb/factory/simple/Factory.java
+++ src/main/java/com/syb/factory/simple/Factory.java
@@ -3,11 +3,19 @@ package com.syb.factory.simple;
 public class Factory  {
 
     public Phone production(Class clz){
-        if(XiaoMiPhone.class == clz){
+        try {
+            if (clz != null){
+                return (Phone) clz.newInstance();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+       /* if(XiaoMiPhone.class == clz){
             return new XiaoMiPhone();
         }else if(HuaweiPhone.class == clz){
             return new HuaweiPhone();
-        }
+        }*/
         return null;
     }
 
